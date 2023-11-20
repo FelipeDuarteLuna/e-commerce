ac1c69ed8b820eef0624eb3d5236e39330dda1fb
Felipe Luna <felipe.luna@totvs.com.br>
2023-11-20 17:22:36 -0300

    feat(layout): add href to logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('home').then((m) => m.HomeModule),
+  },
+];
