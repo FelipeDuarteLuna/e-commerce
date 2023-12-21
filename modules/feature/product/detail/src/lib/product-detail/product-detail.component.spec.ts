d264c0686a42da849c5c10d17cf0b55c72e5542c
Felipe Luna <felipe.luna@totvs.com.br>
2023-12-21 10:40:59 -0300

    feat(product-detail): fetch product by id

--- modules/feature/product/detail/src/lib/product-detail/product-detail.component.spec.ts
+++ modules/feature/product/detail/src/lib/product-detail/product-detail.component.spec.ts
@@ -1,6 +1,8 @@
 import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductDetailComponent } from './product-detail.component';
+import { ProductSearchService, mockProducts } from 'product-data-access';
+import { of } from 'rxjs';
 
 describe('ProductDetailComponent', () => {
   let component: ProductDetailComponent;
@@ -9,6 +11,12 @@ describe('ProductDetailComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductDetailComponent, RouterTestingModule],
+      providers: [
+        {
+          provide: ProductSearchService,
+          useValue: { getById: () => of(mockProducts[0]) },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductDetailComponent);
