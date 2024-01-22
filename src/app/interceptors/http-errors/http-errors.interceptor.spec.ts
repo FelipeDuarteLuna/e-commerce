a3f15bd145409918ba2a316275d0f1f914571fc9
Felipe Luna <felipe.luna@totvs.com.br>
2024-01-22 06:05:57 -0300

    test(ecommerce): add tests to http errors interceptor

--- src/app/interceptors/http-errors/http-errors.interceptor.spec.ts
+++ src/app/interceptors/http-errors/http-errors.interceptor.spec.ts
@@ -1,18 +1,51 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpErrorsInterceptor } from './http-errors.interceptor';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('HttpErrorsInterceptor', () => {
-  beforeEach(() =>
+  let httpClient: HttpClient;
+  let httpMock: HttpTestingController;
+  let matSnackBar: MatSnackBar;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpErrorsInterceptor],
-    })
-  );
+      imports: [HttpClientTestingModule, MatSnackBarModule],
+      providers: [
+        HttpErrorsInterceptor,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: HttpErrorsInterceptor,
+          multi: true,
+        },
+      ],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+    matSnackBar = TestBed.inject(MatSnackBar);
+  });
 
   it('should be created', () => {
     const interceptor: HttpErrorsInterceptor = TestBed.inject(
       HttpErrorsInterceptor
     );
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should open notification on http error', () => {
+    jest.spyOn(matSnackBar, 'open');
+    httpClient.get('/test').subscribe();
+
+    const request = httpMock.expectOne('/test');
+    request.error(new ProgressEvent('error'));
+
+    expect(matSnackBar.open).toHaveBeenCalled();
+    expect(request.request.headers.has('x-access-token')).toBe(true);
   });
 });
