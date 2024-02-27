2ef1ac693186d4f370183abb01bf4ef5b49a7a83
Felipe Luna <felipe.luna@totvs.com.br>
2024-02-27 17:02:23 -0300

    feat(data-access-auth): add auth guard

--- modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-password/auth-form-password.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { AuthFormComponent } from '../auth-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from 'modules/data-access/auth/src/lib/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-auth-form-password',
@@ -13,5 +15,13 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './auth-form-password.component.scss',
 })
 export class AuthFormPasswordComponent {
+  email = inject(AuthFormComponent).form.controls.email.value;
   control = inject(AuthFormComponent).form.controls.password;
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  login(): void {
+    this.authService.setEmail(this.email);
+    this.router.navigate(['/']);
+  }
 }
